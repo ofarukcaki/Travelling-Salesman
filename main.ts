af17ccf273a1ff86bb784f3747f7a5a5a1dbb133
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-06-15 17:44:14 +0300

    read input file & parse

---
+++ main.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import { log } from './src/debug';
+
+// city(node) structure
+interface city {
+  id: number;
+  x: number;
+  y: number;
+}
+
+/* ======== MAIN ======== */
+// array to hold cities vector
+let cities: Array<city> = [];
+
+// text input to be processed
+const inputFile = process.argv[2];
+
+// read the file and split by CRLF and LF linebreaks
+const lines: string[] = fs.readFileSync(inputFile).toString().split(/\r?\n/);
+
+// parse all lines and push to cities array
+for (let i = 0; i < lines.length; i++) {
+  // do not consider the empty line
+  if (lines[i] === '') continue;
+  // split the line by space as a Number
+  const [id, x, y] = lines[i].split(' ').map(Number);
+  // push the new city into cities array
+  cities.push({ id, x, y });
+}
+
+log(cities)
--- src/debug.ts
+++ src/debug.ts
@@ -1,7 +1,7 @@
 // replacement for the console.log() function
 // logs only -D command-line argument is provided
 // will be mostly used for debugging purposes
-export function log(logString: string) {
+export function log(logString: any) {
   if (process.argv.indexOf('-D') !== -1) {
     console.log(logString);
   }
