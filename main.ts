f4fc4065b17de671412a3a3b1666224b9f9d24f7
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-06-17 11:35:16 +0300

    distance matrix fixed

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-import fs from 'fs';
 import { log } from './src/debug';
 import {
   city,
-  distMatrix,
   MST,
   match,
   eulerCitcuit,
@@ -10,6 +8,30 @@ import {
   hamiltonToTSP,
   twoOpt,
 } from './src/TSP';
+const fs = require('fs');
+
+function distanceBetween(city1: city, city2: city) {
+  const distX = city1.x - city2.x;
+  const distY = city1.y - city2.y;
+  const res = Math.round(Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2)));
+  // console.log(`Dist between ${city1.x},${city1.y} and ${city2.x},${city2.y} is ${res}`);
+  return res;
+}
+
+function distMatrix(cities: Array<city>) {
+  let matrix: Array<Array<number>> = new Array(cities.length);
+  for (let i = 0; i < cities.length; i++) {
+    matrix[i] = new Array(cities.length);
+    for (let j = 0; j < cities.length; j++) {
+      // get distance between twi cities
+      const distance = distanceBetween(cities[i], cities[j]);
+      // add into distance matrix
+      // matrix[i].push(distance);
+      matrix[i][j] = distance;
+    }
+  }
+  return matrix;
+}
 
 /* ======== MAIN ======== */
 // array to hold cities vector
@@ -26,38 +48,74 @@ for (let i = 0; i < lines.length; i++) {
   // do not consider the empty line
   if (lines[i] === '') continue;
   // split the line by space as a Number
-  const [id, x, y] = lines[i].split(' ').map(Number);
+  const [id, x, y] = lines[i].trim().split(/\s+/).map(Number);
   // push the new city into cities array
   cities.push({ id, x, y });
 }
 
-log(cities);
+// log(cities);
+// cities.forEach((x) => {
+//   console.log(x.id, x.x, x.y);
+// });
 
 //Create distnace matrix from cities
 const matrix = distMatrix(cities);
-log(matrix);
-
+// console.log(matrix[0]);
+// console.log(matrix);
+// log(matrix);
+// console.log('Length:', cities.length);
 //Create minimum spanning tree adjacency list
 const adjacencyList = MST(matrix);
-log(adjacencyList);
+// log(adjacencyList);
+// adjacencyList.forEach((x) => {
+//   // console.log(x);
+//   let s = '';
+//   x.forEach(y => {
+//     s += y + " ";
+//   })
+//   console.log(s);
+// });
 
-log('--------------');
+// log('--------------');
 
 match(adjacencyList, matrix);
 
-log(adjacencyList);
+// process.exit();
+
+// log(adjacencyList);
 
 const euler: Array<number> = eulerCitcuit(adjacencyList);
-log(`Euler path: ${euler}`);
+
+// console.log(`Euler path: ${euler}`);
+// console.log(euler);
 
 const hamilton: Array<number> = convertHamilton(euler);
 log(`Hamilton: ${hamilton}`);
+// console.log(`Hamilton: ${hamilton}`);
 
 let tsp: Array<city> = [];
 let cost = hamiltonToTSP(cities, matrix, hamilton, tsp);
 
-log(`cost: ${cost}`)
-log(tsp)
+console.log(`cost: ${cost}`);
+log(tsp);
 
-cost = twoOpt(tsp);
-console.log("cost after 2-opt: ", cost);
+// if (cities.length < 3000) {
+//   cost = twoOpt(tsp);
+//   console.log('cost after 2-opt: ', cost);
+// }
+
+// prepare the output to print instead writing to a file each time in a loop
+// because it is way more slower sicnde it requires to open/close file in each loop
+let stringToWrite: string = '';
+tsp.forEach((city) => {
+  stringToWrite += city.id + '\n';
+});
+
+// console.log(stringToWrite);
+// set outpur file name
+const outputFile = inputFile.replace('.txt', '.out');
+
+// writefilesync method deletes the file if it is already available
+// write the cost of the TSP problem to the first line
+fs.writeFileSync(outputFile, cost + '\n');
+fs.appendFileSync(outputFile, stringToWrite);
--- src/TSP.ts
+++ src/TSP.ts
@@ -1,5 +1,4 @@
 import { log } from './debug';
-import { cpuUsage } from 'process';
 // city(node) structure
 export interface city {
   id: number;
@@ -7,28 +6,15 @@ export interface city {
   y: number;
 }
 
-// Construct a distance matrix of cities
-export function distMatrix(cities: Array<city>) {
-  let matrix: Array<Array<number>> = new Array(cities.length);
-
-  for (let i = 0; i < cities.length; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < cities.length; j++) {
-      // get distance between twi cities
-      const distance = distanceBetween(cities[i], cities[j]);
-      // add into distance matrix
-      matrix[i].push(distance);
-    }
-  }
-  return matrix;
-}
-
 // distance between two cities
 function distanceBetween(city1: city, city2: city) {
   const distX = city1.x - city2.x;
   const distY = city1.y - city2.y;
-  return Math.round(Math.sqrt(distX ** 2 + distY ** 2));
+  const res= Math.round(Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2)));
+  // console.log(`Dist between ${city1.x},${city1.y} and ${city2.x},${city2.y} is ${res}`);
+  return res;
 }
+
 
 // let city1 = { id: 1, x: 0, y: 2 };
 // let city2 = { id: 2, x: 2, y: 3 };
